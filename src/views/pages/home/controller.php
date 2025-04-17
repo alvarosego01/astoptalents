07106f9aed8f6411acf7f19920a72c805305bf94
Alvaro Segovia <alvarosego01@gmail.com>
2025-04-17 12:42:57 -0400

    Refactor code structure for improved readability and maintainability

--- src/views/pages/home/controller.php
+++ src/views/pages/home/controller.php
@@ -51,7 +51,7 @@ class HomeController extends PageBaseController
       [
         "title" => "Andrés Valverde",
         "subTitle" => "developer",
-        "desc" => "Focused on clean code, scalable solutions, and efficient web & app developmen",
+        "desc" => "Focused on clean code, scalable solutions,<br> and efficient web & app development<br><br>",
         "img" => $this->general_functions->get_file('/images/talent_person.png'),
         "subTag" => '$900 / month'
       ],
@@ -70,6 +70,102 @@ class HomeController extends PageBaseController
   }
 
   function setChooseSlider(){
+    $slides = array(
+      [
+        'image' => [
+          'src' => $this->general_functions->get_file('/images/slider_0.png'),
+          'alt' => 'Your full service',
+        ],
+        'info' => [
+          'icon' => "<img alt='icon slider' class='w-auto h-14' src='". $this->general_functions->get_file(' /images/icons/icon_together.png') ."'>",
+          'title' => "Your Full-Service<br> Talent Partner",
+          'desc' => "We manage all aspects of hiring—including<br> recruiting, vetting, compliance, and global payroll<br>—allowing you to focus on business growth"
+        ],
+        'button' => [
+          'text' => 'Learn more',
+          'link' => '/#'
+        ]
+      ],
+      [
+        'image' => [
+          'src' => $this->general_functions->get_file('/images/slider_1.png'),
+          'alt' => 'Your full service',
+        ],
+        'info' => [
+          'icon' => "<img alt='icon slider' class='w-auto h-14' src='". $this->general_functions->get_file(' /images/icons/icon_laptop_person.png') ."'>",
+          'title' => "Immersive Virtual<br> Office Technology",
+          'desc' => "Boost remote work with virtual office spaces<br> designed for collaboration, productivity, and stronger<br> team connections"
+        ],
+        'button' => [
+          'text' => 'Learn more',
+          'link' => '/#'
+        ]
+      ],
+      [
+        'image' => [
+          'src' => $this->general_functions->get_file('/images/slider_2.png'),
+          'alt' => 'Your full service',
+        ],
+        'info' => [
+          'icon' => "<img alt='icon slider' class='w-auto h-14' src='". $this->general_functions->get_file(' /images/icons/icon_check_2.png') ."'>",
+          'title' => "Your Satisfaction,<br> Guaranteed",
+          'desc' => "Your success is our priority. If your hire doesn’t align<br> perfectly with your expectations, we’ll promptly<br> provide a replacement to ensure you’re fully satisfied."
+        ],
+        'button' => [
+          'text' => 'Learn more',
+          'link' => '/#'
+        ]
+      ],
+      [
+        'image' => [
+          'src' => $this->general_functions->get_file('/images/slider_3.png'),
+          'alt' => 'Your full service',
+        ],
+        'info' => [
+          'icon' => "<img alt='icon slider' class='w-auto h-14' src='". $this->general_functions->get_file(' /images/icons/icon_check.png') ."'>",
+          'title' => "Clear and<br> Transparent Pricing",
+          'desc' => "Transparent pricing with no hidden costs ensures<br> risk-free hiring; you pay only when you find the<br>  ideal candidate."
+        ],
+        'button' => [
+          'text' => 'Learn more',
+          'link' => '/#'
+        ]
+      ],
+      [
+        'image' => [
+          'src' => $this->general_functions->get_file('/images/slider_4.png'),
+          'alt' => 'Your full service',
+        ],
+        'info' => [
+          'icon' => "<img alt='icon slider' class='w-auto h-14' src='". $this->general_functions->get_file(' /images/icons/icon_search_people.png') ."'>",
+          'title' => "Access Trusted<br> Top Talent",
+          'desc' => "Access top LATAM professionals remotely &<br> cut hiring costs without compromising quality.."
+        ],
+        'button' => [
+          'text' => 'Learn more',
+          'link' => '/#'
+        ]
+      ],
+      [
+        'image' => [
+          'src' => $this->general_functions->get_file('/images/slider_5.png'),
+          'alt' => 'Your full service',
+        ],
+        'info' => [
+          'icon' => "<img alt='icon slider' class='w-auto h-14' src='". $this->general_functions->get_file(' /images/icons/icon_protect.png') ."'>",
+          'title' => "No Compliance<br> Worries",
+          'desc' => "We manage all legal and compliance requirements,<br> strictly adhering to local employment regulations<br> across all operating countries"
+        ],
+        'button' => [
+          'text' => 'Learn more',
+          'link' => '/#'
+        ]
+      ],
+    );
+
+    $this->add_to_context([
+      'choose_slider' => $slides,
+    ]);
 
   }
 
